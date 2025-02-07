e21ac8e1b9bd4e6918ae0060eafe81d23d407033
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-02-07 11:22:18 +0100

    moved back to static - God it was hard

--- build.sh
+++ build.sh
@@ -10,9 +10,15 @@
 pip install --upgrade pip setuptools setuptools_scm[toml] build twine # upgrade essential tools and install build and twine
 #pip install --upgrade build twine setuptools-scm
 #python -m build
+
+red '1. test SCM - see code..'
+#SETUPTOOLS_SCM_DEBUG=1 python -m build
+
 yellow "2. build with Mad the Twine.."
 
-python -m build -v
+
+python -m build --no-isolation
+#python -m build -v
 #python -m build --wheel
 #python -m build --sdist
 
